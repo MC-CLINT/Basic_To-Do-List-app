781d763edabe18aae961448d701c94be151b299d
MC-CLINT <ckvidam@st.ug.edu.gh>
2024-11-17 12:04:07 +0000

    Adding functions. Next- function to save data

---
+++ script.js
@@ -0,0 +1,38 @@
+const inputArea = document.getElementById('input-area');
+const listContainer = document.getElementById('list-container');
+
+
+function addTask(){
+    if (inputArea.value === ''){
+        alert('You have to type something')
+    }
+    else {
+        let li=document.createElement("li")
+        li.innerHTML = inputArea.value;
+        listContainer.appendChild(li);
+
+        // adding cross or delete sign by the list
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
+    }
+    inputArea.value = ""
+}
+
+
+listContainer.addEventListener("click", function(e){
+    if (e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+    }
+
+ else if (e.target.tagName === 'SPAN'){
+    e.target.parentElement.remove();
+ } 
+}, false);
+
+
+// Storing Tasks in our browser so that after refreshing the page, we have all tasks saved
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML)
+}
